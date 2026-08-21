55e7ed8f2a13eb39799e670430d3018fc8da0a35
exuprys <exuprys@gmail.com>
2026-08-21 20:55:43 +0100

    update loginWithGoogle baseURL to point to the correct server port

--- src/api/axios.js
+++ src/api/axios.js
@@ -91,7 +91,7 @@ export const login = async (credentials) => {
 
 // Redirige vers Laravel qui redirige vers Google
 export const loginWithGoogle = () => {
-    const baseURL = "http://localhost:8000";
+    const baseURL = "http://localhost:8001";
     window.location.href = `${baseURL}/auth/google/redirect`;
 };
 
